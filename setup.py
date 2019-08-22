52ed828cf76a1716e5e2c2aa00455f6a3b49daa5
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2019-08-22 14:36:27 +0300

    Udpate setup

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except:
+    logger.error('setuptools is not installed or outdated!\n\n'
+                 'You can install or update setuptools using\n'
+                 'pip install --upgrade setuptools (if you have pip)\n'
+                 'or\n'
+                 'sudo apt-get install python-setuptools (on Ubuntu)\n',
+                 exit_with_code=1)
+
 setup(name='orf-search',
       version='0.4',
       author='Tatiana Dvorkina',
       author_email='tanunia@gmail.com',
       url='https://github.com/ablab/orf-search',
-      packages = ['','scripts']
+      license='GPLv2',
+      packages=['','scripts'],
+      install_requires=[
+        'joblib',
+        'edlib',
+        'argparse',
+        'pyyaml',
+        'biopython'
+      ],
+      include_package_data = True
       )
