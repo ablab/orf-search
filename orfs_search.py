090dbe32abed63766f211a0be743d9b808048997
Tatiana Dvorkina <tdvorkina@chihua.cab.spbu.ru>
2019-09-19 23:25:40 +0300

    save tiny dataset output

--- orfs_search.py
+++ orfs_search.py
@@ -93,9 +93,10 @@ def main(args):
     is_test = False
     if len(args) == 2 and args[1] == "--test":
         is_test = True
-        tmp_dir = tempfile.TemporaryDirectory()
+        test_dir = "tiny_dataset_test"
         args = [args[0], "-m", PKG_LOCATION  + "/tiny_dataset/ricinb_lectin2.hmm", "-s", PKG_LOCATION  + "/tiny_dataset/toxin.fasta", \
-                         "-r", "-g", PKG_LOCATION  + "/tiny_dataset/graph.gfa", "-k", "55", "-o", tmp_dir.name]
+                         "-r", "-g", PKG_LOCATION  + "/tiny_dataset/graph.gfa", "-k", "55", "-o", test_dir]
+
     args = parser.parse_args(args[1:])
 
     logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG)
@@ -150,8 +151,6 @@ def main(args):
 
     if is_test:
         logging.info(u'The test finished successfully!')
-        tmp_dir.cleanup()
-
 
 
 if __name__ == "__main__":
